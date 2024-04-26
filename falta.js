8727c7f6362ad6fc82feb13c90a28f53c6bc91a3
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2024-04-26 12:47:46 -0300

    Pop-up do golpe em desenvolvimento
    Conversar com o Pedro sobre algumas funcionalidades

--- falta.js
+++ falta.js
@@ -6,6 +6,10 @@ const abrirgolpe = document.querySelector("#botaogolpe");
 const modalgolpe = document.querySelector("#popgolpe");
 const fechargolpe = document.querySelector("#popgolpe #fechar");
 
+const direita = document.querySelector("#boataodireita");
+const modaldireita = document.querySelector("#popdireita");
+const fechardireita = document.querySelector("#popdireita #fechar")
+
 button.onclick = function () {
   modalfalta.showModal();
 };
@@ -20,3 +24,11 @@ abrirgolpe.onclick = function () {
 fechargolpe.onclick = function () {
   modalgolpe.close();
 };
+
+direita.onclick = function () {
+  modaldireita.showModal();
+};
+fechardireita.onclick = function () {
+  modaldireita.close();
+}
+
